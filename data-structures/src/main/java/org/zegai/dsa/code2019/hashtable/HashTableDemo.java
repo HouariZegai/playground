1eeb33302500794c6b20657bfd14a7b703f47766
HouariZegai <houarizegai14@gmail.com>
2025-09-30 08:14:01 +0100

    refactor: restructure dsa package

---
+++ data-structures/src/main/java/org/zegai/dsa/code2019/hashtable/HashTableDemo.java
@@ -0,0 +1,11 @@
+package org.zegai.dsa.code2019.hashtable;
+
+public class HashTableDemo {
+    public static void main(String[] args) {
+        HashTableArray<String> hashTableArray = new HashTableArray<>(5);
+        hashTableArray.put(1, "Houari");
+        hashTableArray.put(6, "ZEGAI");
+        hashTableArray.put(2, "Worker");
+        System.out.println(hashTableArray.get(6));
+    }
+}
